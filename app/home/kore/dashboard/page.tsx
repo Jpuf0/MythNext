6c756464f2274c048db2068c259883def90d3c47
MythicXGN <hydythediny@gmail.com>
2023-05-06 19:11:31 -0400

    Unstable zlib not working

--- app/home/kore/dashboard/page.tsx
+++ app/home/kore/dashboard/page.tsx
@@ -1,20 +1,28 @@
-import '../../../src/styles/styles.css';
-import '../../../src/styles/bootstrap.css';
-import '../../../src/styles/modesta.css';
+const config = require('../../../../config.js');
 
-import FooterComponent from '@/public/footer';
+const {Client, Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.login(config.discord.bot_token);
 
-function Dashboard() {
-  return (
-    <div className="Dashboard">
-        <div className="Home">
-            <div className="fullscreen">
-              
-            </div>
-        </div>
-      <FooterComponent></FooterComponent>
-    </div>
-  );
-}
+const DarkDashboard = require('dbd-dark-dashboard');
+const DBD = require("discord-dashboard");
+let langsSettings = {};
 
-export default Dashboard;
+(async ()=>{
+    let DBD = require('discord-dashboard');
+    await DBD.useLicense(config.dbd_license);
+    DBD.Dashboard = DBD.UpdatedClass();
+
+    const Dashboard = new DBD.Dashboard({
+        port: 3000,
+        client: {
+            id: config.discord.client_id,
+            secret: config.discord.client_secret
+        },
+        redirectUri: config.redirect_uri,
+        domain: 'http://localhost/home/kore/dashboard',
+        bot: client,
+        theme: DarkDashboard(DBD.default_configs.dbdDarkDashboard),
+    });
+    Dashboard.init();
+})();
